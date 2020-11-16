3b367f0c69b6c52e2e2caa0b644f8b64a488ba59
rpsutton <rpsiii99@gmail.com>
2020-11-16 12:38:04 -0500

    lifecycle update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,19 +3,26 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Spinner } from 'react-bootstrap';
 
 function App() {
 
   const [renderList, setRenderList] = useState(false);
   const [businesses, setBusinesses] = useState([]);
+  const [loading, setLoading] = useState(false);
 
 
   function searchYelp(term, location, sortBy) {
+    setLoading(true);
     Yelp.search(term, location, sortBy).then(businesses => {
-      setBusinesses(businesses)
+      setBusinesses(businesses);
     })
-    .then(setRenderList(true))
+    .then(() => {
+      setRenderList(true);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
   }
 
   function renderBusinessList() {
@@ -24,10 +31,17 @@ function App() {
     }
   }
 
+  function renderSpinner() {
+    if (loading) {
+      return <Spinner animation="border" variant="light"/>
+    }
+  }
+
   return (
     <div className="App">
       <Navbar expand="lg" variant="dark" bg="primary" className="d-flex justify-content-center align-items-center">
       <Navbar.Brand><h2>Let's Eat</h2></Navbar.Brand>
+      {renderSpinner()}
       </Navbar>
       <SearchBar searchYelp={searchYelp} />
       <section id="businessList">
